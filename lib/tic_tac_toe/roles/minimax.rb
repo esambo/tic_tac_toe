c0d64dfe6052ff6b60ba6ed8b93a9d9fd3565438
E. Sambo <news@emmanuelsambo.com>
2012-03-26 20:58:07 -0500

    Refactor: Extract method Minmax#best

--- lib/tic_tac_toe/roles/minimax.rb
+++ lib/tic_tac_toe/roles/minimax.rb
@@ -7,28 +7,10 @@ module TicTacToe
       depth ||= ply_number
       return final_position(depth) if self.terminal?
       final_positions = successors(depth)
-      if self.next_player.X?
-        best = max_for_X(final_positions)
-      else
-        best = min_for_O(final_positions)
-      end
-      best.position_number = self.last_position_number
-      best
+      best(final_positions)
     end
 
     private
-
-      def successors(depth)
-        final_positions = []
-        self.empty_spaces.each do |s|
-          self.place_mark_at_index(s)
-          self.take_turn
-          final_positions << self.best_position(depth + 1)
-          self.take_turn
-          self.undoo_mark_at_index(s)
-        end
-        final_positions
-      end
 
       def ply_number
         self.positions.count { |p| !p.none? }
@@ -43,17 +25,40 @@ module TicTacToe
         end
       end
 
-      def rank(winner, depth)
-        (self.size - depth + 1) * winner.to_i
+        def rank(winner, depth)
+          (self.size - depth + 1) * winner.to_i
+        end
+
+      def successors(depth)
+        final_positions = []
+        self.empty_spaces.each do |s|
+          self.place_mark_at_index(s)
+          self.take_turn
+          final_positions << self.best_position(depth + 1)
+          self.take_turn
+          self.undoo_mark_at_index(s)
+        end
+        final_positions
       end
 
-      def max_for_X(final_positions)
-        final_positions.max_by { |p| p.rank }
+      def best(final_positions)
+        best = nil
+        if self.next_player.X?
+          best = max_for_X(final_positions)
+        else
+          best = min_for_O(final_positions)
+        end
+        best.position_number = self.last_position_number
+        best
       end
 
-      def min_for_O(final_positions)
-        final_positions.min_by { |p| p.rank }
-      end
+        def max_for_X(final_positions)
+          final_positions.max_by { |p| p.rank }
+        end
+
+        def min_for_O(final_positions)
+          final_positions.min_by { |p| p.rank }
+        end
 
   end
 end
